80a688363d8f562bfaf4c73f80306f8270693280
chrisdanielwise <chrisdanielwise@gmail.com>
2023-06-27 00:42:11 +0100

    made some fix

--- 0x00-ES6_basic/100-main.js
+++ 0x00-ES6_basic/100-main.js
@@ -1,7 +1,6 @@
-import createIteratorObject from "./100-createIteratorObject.js";
-
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
+import createIteratorObject from "./100-createIteratorObject";
+import createEmployeesObject from './11-createEmployeesObject';
+import createReportObject from './12-createReportObject';
 
 const employees = {
     ...createEmployeesObject('engineering', ['Bob', 'Jane']),
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,4 @@
-export default function returnHowManyArguments(...rest) {
-    return rest.length;
-}
+export default function returnHowManyArguments(...args) {
+    return args.length;
+  }
+  
