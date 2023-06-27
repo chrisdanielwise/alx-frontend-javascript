abacf4aa50e42d5c05709827f560f0d8fca3d97d
chrisdanielwise <chrisdanielwise@gmail.com>
2023-06-27 01:59:39 +0100

    fix all files

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
+function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      return [task, task2];
+    }
+  
+    return [task, task2];
   }
-
-  return [task, task2];
-}
+  
+  export default taskBlock;
+  
