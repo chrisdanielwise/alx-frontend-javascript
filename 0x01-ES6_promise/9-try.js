a4d65ce087ee641f3f52f373d83c05c4c303cb3c
chrisdanielwise <chrisdanielwise@gmail.com>
2023-06-27 21:23:11 +0100

    made some changes in file

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,29 +1,14 @@
-// export default function guardrail(mathFunction) {
-//   const queue = [];
-
-//   try {
-//     const result = mathFunction();
-//     queue.push(result);
-//   } catch (error) {
-//     queue.push(error.message);
-//   } finally {
-//     queue.push('Guardrail was processed');
-//   }
-
-//   return queue;
-// }
-
-function guardrail(mathFunction) {
+export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    queue.push(mathFunction());
-  } catch (err) {
-    queue.push(err.toString());
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
   } finally {
     queue.push('Guardrail was processed');
   }
+
   return queue;
 }
-
-export default guardrail;
